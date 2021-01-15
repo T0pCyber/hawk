2d7a63f0fbc81ae80e5b3c448774641bdbdce012
Paul Navarro <paul_nav14@yahoo.com>
2021-01-15 12:40:02 -0500

    Fix Analyzer

--- Hawk/tests/general/PSScriptAnalyzer.Tests.ps1
+++ Hawk/tests/general/PSScriptAnalyzer.Tests.ps1
@@ -2,7 +2,7 @@
 Param (
 	[switch]
 	$SkipTest,
-	
+
 	[string[]]
 	$CommandPath = @("$global:testroot\..\functions", "$global:testroot\..\internal\functions")
 )
@@ -12,21 +12,21 @@ if ($SkipTest) { return }
 $global:__pester_data.ScriptAnalyzer = New-Object System.Collections.ArrayList
 
 Describe 'Invoking PSScriptAnalyzer against commandbase' {
-	$commandFiles = Get-ChildItem -Path $CommandPath -Recurse | Where-Object Name -like "*.ps1"
+	$commandFiles = Foreach ($file in $CommandPath) {Get-ChildItem -Path $file -Recurse | Where-Object Name -like "*.ps1"}
 	$scriptAnalyzerRules = Get-ScriptAnalyzerRule
-	
+
 	foreach ($file in $commandFiles)
 	{
 		Context "Analyzing $($file.BaseName)" {
 			$analysis = Invoke-ScriptAnalyzer -Path $file.FullName -ExcludeRule PSAvoidTrailingWhitespace, PSShouldProcess
-			
+
 			forEach ($rule in $scriptAnalyzerRules)
 			{
 				It "Should pass $rule" -TestCases @{ analysis = $analysis; rule = $rule } {
 					If ($analysis.RuleName -contains $rule)
 					{
 						$analysis | Where-Object RuleName -EQ $rule -outvariable failures | ForEach-Object { $null = $global:__pester_data.ScriptAnalyzer.Add($_) }
-						
+
 						1 | Should -Be 0
 					}
 					else
