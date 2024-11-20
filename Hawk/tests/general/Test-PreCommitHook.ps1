ea16c4be08269c8b6f3fd86df455c0387b6cf457
Jonathan Butler <xxbutler86xx@gmail.com>
2024-11-20 15:51:25 -0500

    Change PSScriptAnalyzerSettings.psd1 path to not include GitHub workspace environment variable.

--- Hawk/tests/general/Test-PreCommitHook.ps1
+++ Hawk/tests/general/Test-PreCommitHook.ps1
@@ -5,7 +5,7 @@
 # 3. Observe the PSScriptAnalyzer warnings/errors
 
 #region Good Code Examples - These will pass PSScriptAnalyzer
-# add comment to testss
+# add comment to tests
 function Test-GoodFunction {
     [CmdletBinding()]
     param (
