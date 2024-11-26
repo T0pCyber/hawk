c81824150f8293c8276c72ee4ff82ff44a6ec560
Jonathan Butler <xxbutler86xx@gmail.com>
2024-11-26 12:22:47 -0500

    Test good workflow once more.

--- build/vsts-prerequisites.ps1
+++ build/vsts-prerequisites.ps1
@@ -10,17 +10,17 @@ $modules = @("Pester", "PSFramework", "PSModuleDevelopment")
 # Automatically add missing dependencies
 # TODO: uncomment this block of code below and fix RobustCloudCommand error.
 
-# $data = Import-PowerShellDataFile -Path "$PSScriptRoot\..\Hawk\Hawk.psd1"
-# foreach ($dependency in $data.RequiredModules) {
-#     if ($dependency -is [string]) {
-#         if ($modules -contains $dependency) { continue }
-#         $modules += $dependency
-#     }
-#     else {
-#         if ($modules -contains $dependency.ModuleName) { continue }
-#         $modules += $dependency.ModuleName
-#     }
-# }
+$data = Import-PowerShellDataFile -Path "$PSScriptRoot\..\Hawk\Hawk.psd1"
+foreach ($dependency in $data.RequiredModules) {
+    if ($dependency -is [string]) {
+        if ($modules -contains $dependency) { continue }
+        $modules += $dependency
+    }
+    else {
+        if ($modules -contains $dependency.ModuleName) { continue }
+        $modules += $dependency.ModuleName
+    }
+}
 
 foreach ($module in $modules) {
     # Write-Output "Installing module: $module"
