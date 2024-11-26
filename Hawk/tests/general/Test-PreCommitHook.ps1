5a1cc147a522fbc4652b64696936bb3eb0c703ea
Jonathan Butler <xxbutler86xx@gmail.com>
2024-11-26 11:58:58 -0500

    Update upload results to upload file even if no bad results are found

--- Hawk/tests/general/Test-PreCommitHook.ps1
+++ Hawk/tests/general/Test-PreCommitHook.ps1
@@ -5,7 +5,7 @@
 # 3. Observe the PSScriptAnalyzer warnings/errors
 
 #region Good Code Examples - These will pass PSScriptAnalyzer
-# testsssss
+# testssssss
 function Test-GoodFunction {
     [CmdletBinding()]
     param (
