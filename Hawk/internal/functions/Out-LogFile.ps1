35a767c2bb65c7a5e336a8ae9b679fe050befdcd
Jonathan Butler <xxbutler86xx@gmail.com>
2024-12-19 16:04:15 -0500

    Modify Out-LogFile to use the same formatted log tags as other logs, using brackets. Created an internal function to test for suspicious inbox rules, which can be called by all current and future functions that pull inbox rule data (e.g. Get-HawkTenantAdminInboxRuleModification, Get-HawkTenantAdminInboxRuleHistory). Modify Get-AllUnifiedAuditLogEntry to not use invoke-expression as this is a security issue and is flagged by PSSA

--- Hawk/internal/functions/Out-LogFile.ps1
+++ Hawk/internal/functions/Out-LogFile.ps1
@@ -1,20 +1,57 @@
 ﻿Function Out-LogFile {
     <#
     .SYNOPSIS
-        Writes output to a log file with a time date stamp
+        Writes output to a log file with a time date stamp.
     .DESCRIPTION
         Writes output to a log file with a time date stamp and appropriate prefixes
-        based on the type of message (action, notice, etc.)
+        based on the type of message (action, notice, etc.). By default, messages are
+        also displayed on the screen unless the -NoDisplay switch is used.
+
     .PARAMETER string
-        Log Message
+        The log message to be written.
+
     .PARAMETER action
-        Switch indicating an action is being performed
+        Switch indicating the log entry is describing an action being performed.
+
     .PARAMETER notice
-        Switch indicating this is a notice that requires investigation
+        Switch indicating the log entry requires investigation or special attention.
+
     .PARAMETER silentnotice
-        Switch indicating this is additional information for an investigation notice
+        Switch indicating additional investigative information that should not be
+        displayed on the screen. This is logged to the file but suppressed in console output.
+
     .PARAMETER NoDisplay
-        Switch indicating the message should only be written to the log file, not displayed
+        Switch indicating the message should only be written to the log file,
+        not displayed in the console.
+
+    .EXAMPLE
+        Out-LogFile "Routine scan completed."
+
+        Writes a simple log message with a timestamp to the log file and displays it on the screen.
+
+    .EXAMPLE
+        Out-LogFile "Starting mailbox export operation" -action
+
+        Writes a log message indicating an action is being performed. 
+        The output is prefixed with [ACTION] in the log file.
+
+    .EXAMPLE
+        Out-LogFile "Detected suspicious login attempt from external IP" -notice
+
+        Writes a log message indicating a situation requiring investigation. 
+        The output is prefixed with [INVESTIGATE] and also recorded in a separate _Investigate.txt file.
+
+    .EXAMPLE
+        Out-LogFile "User mailbox configuration details" -silentnotice
+
+        Writes investigative detail to the log and _Investigate.txt file without printing to the console. 
+        This is useful for adding detail to a previously logged [INVESTIGATE] event without cluttering the console.
+
+    .EXAMPLE
+        Out-LogFile "Executing periodic health check" -NoDisplay
+
+        Writes a log message to the file without displaying it on the console, 
+        useful for routine logging that doesn't need immediate user visibility.
     #>
     [CmdletBinding()]
     Param
