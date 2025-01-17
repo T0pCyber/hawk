70f9557018f529d15c27d2b10fbc30bc17c9933d
Jonathan Butler <xxbutler86xx@gmail.com>
2025-01-17 15:16:18 -0500

    Update comment based help to pass validate test.

--- Hawk/internal/functions/Test-OperationEnabled.ps1
+++ Hawk/internal/functions/Test-OperationEnabled.ps1
@@ -1,26 +1,25 @@
-<#
-.SYNOPSIS
-    Test if a user has a specific operation enabled for auditing
-.DESCRIPTION
-    Test if a user has a specific operation enabled for auditing
-.EXAMPLE
-    [bool]$result = Test-OperationEnabled -User bsmith@contoso.com -Operation 'SearchQueryInitiated'
-.PARAMETER User
-    Specific user under investigation
-.PARAMETER Operation
-    Operation to be verified enabled for auditing
-.EXAMPLE
-    Test-OperationEnabled -User bsmith@contoso.com -Operation 'SearchQueryInitiated'
-    Checks if the SearchQueryInitiated audit operation is enabled for user bsmith@contoso.com. Returns True if enabled, False if disabled.
-.OUTPUTS
-    System.Boolean
-    Output is a boolean result returned to the calling external function
-.NOTES
-    This function is internal and to be called from another Hawk user-enabled function.  Return value is boolean, and
-    it is intended to be used in an if-else check verifying an operation is enabled for auditing under a given user.
-#>
 Function Test-OperationEnabled {
-    
+    <#
+    .SYNOPSIS
+        Test if a user has a specific operation enabled for auditing
+    .DESCRIPTION
+        Test if a user has a specific operation enabled for auditing
+    .EXAMPLE
+        [bool]$result = Test-OperationEnabled -User bsmith@contoso.com -Operation 'SearchQueryInitiated'
+    .PARAMETER User
+        Specific user under investigation
+    .PARAMETER Operation
+        Operation to be verified enabled for auditing
+    .EXAMPLE
+        Test-OperationEnabled -User bsmith@contoso.com -Operation 'SearchQueryInitiated'
+        
+    .OUTPUTS
+        System.Boolean
+        Output is a boolean result returned to the calling external function
+    .NOTES
+        This function is internal and to be called from another Hawk user-enabled function.  Return value is boolean, and
+        it is intended to be used in an if-else check verifying an operation is enabled for auditing under a given user.
+    #>
     [CmdletBinding()]
     [OutputType([bool])]
     param(
