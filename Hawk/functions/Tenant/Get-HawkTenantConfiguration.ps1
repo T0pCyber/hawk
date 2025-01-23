7e6eaee05e908637dbe62af1c4b161c2409a6b39
Jonathan Butler <xxbutler86xx@gmail.com>
2025-01-23 14:55:32 -0500

    Update comment based help removing referecne to XML file generation in Get-HawkTenantConfiguration

--- Hawk/functions/Tenant/Get-HawkTenantConfiguration.ps1
+++ Hawk/functions/Tenant/Get-HawkTenantConfiguration.ps1
@@ -20,41 +20,21 @@
 	Path: \
 	Description: Output of Get-AdminAuditlogConfig
 
-	File: AdminAuditLogConfig.xml
-	Path: \XML
-	Description: Output of Get-AdminAuditlogConfig as CLI XML
-
 	File: OrgConfig.txt
 	Path: \
 	Description: Output of Get-OrganizationConfig
-
-	File: OrgConfig.xml
-	Path: \XML
-	Description: Output of Get-OrganizationConfig as CLI XML
 
 	File: RemoteDomain.txt
 	Path: \
 	Description: Output of Get-RemoteDomain
 
-	File: RemoteDomain.xml
-	Path: \XML
-	Description: Output of Get-RemoteDomain as CLI XML
-
 	File: TransportRules.txt
 	Path: \
 	Description: Output of Get-TransportRule
 
-	File: TransportRules.xml
-	Path: \XML
-	Description: Output of Get-TransportRule as CLI XML
-
 	File: TransportConfig.txt
 	Path: \
 	Description: Output of Get-TransportConfig
-
-	File: TransportConfig.xml
-	Path: \XML
-	Description: Output of Get-TransportConfig as CLI XML
 .NOTES
 	TODO: Put in some analysis ... flag some key things that we know we should
 #>
